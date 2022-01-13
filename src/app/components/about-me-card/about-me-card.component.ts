44e2605527d403729a2b8af57fa24983a6cac12d
attrix <lucho.bernal@hotmail.com>
2022-01-13 11:52:26 -0300

    cv 2022

--- src/app/components/about-me-card/about-me-card.component.ts
+++ src/app/components/about-me-card/about-me-card.component.ts
@@ -2,35 +2,29 @@ import { Profile } from '../../class/profile';
 import { GithubService } from '../../services/github.service';
 import { Component, OnInit } from '@angular/core';
 
-
-
 @Component({
   selector: 'app-about-me-card',
   templateUrl: './about-me-card.component.html',
-  styleUrls: ['./about-me-card.component.scss']
+  styleUrls: ['./about-me-card.component.scss'],
 })
 export class AboutMeCardComponent implements OnInit {
-
-  
-  public profile:any;
-  public linkedinURL:string
-  public linkCV:string = 'https://drive.google.com/file/d/1GO4cBvlyghIyTNts3-UVQC_c-DuEzoEO/view?usp=sharing'
+  public profile: any;
+  public linkedinURL: string;
+  public linkCV: string;
 
   constructor(private perfGitHub: GithubService) {
-
-    this.profile = ''
-    this.linkedinURL = "https://www.linkedin.com/in/luciano-sinisterra-4a413913b/"
-   }
+    this.linkCV ='https://drive.google.com/file/d/1eLt-cvRD_par698JdlCLSoKjOOKR4Azx/view?usp=sharing';
+    this.profile = '';
+    this.linkedinURL ='https://www.linkedin.com/in/luciano-sinisterra-4a413913b/';
+  }
 
   ngOnInit(): void {
     this.myProfileGitHub();
   }
 
-  public myProfileGitHub(){
-    this.perfGitHub.getProfileGitHub().subscribe((data)=>{
-      this.profile=data;
-
+  public myProfileGitHub() {
+    this.perfGitHub.getProfileGitHub().subscribe((data) => {
+      this.profile = data;
     });
   }
-
 }
